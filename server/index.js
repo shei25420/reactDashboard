e6ce120504f9ad8d397aa485804a7d3ffe4c0cb8
shei25420 <chegeeric66@gmail.com>
2023-01-15 00:12:07 +0300

    Server & Client Skeleton

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Aoutes
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
+
+// DB CONNECTION
+const PORT = process.env.PORT || 9000;
+const MONGOURL = process.env.MONGO_URL;
+mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch((err) => console.log(`${err} did not connect`));
